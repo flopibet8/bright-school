d40869f4ecfd5363dcd9446777b27fee24a24833
Mujeeb <mujeebjaved8@gmail.com>
2026-08-24 22:58:55 +0500

    Add main execution entrypoint for Railway

--- main.py
+++ main.py
@@ -247,3 +247,8 @@ def root():
         </body>
     </html>
     """
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
